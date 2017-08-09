7092ce94a537c5f35d1916833e14ecb6cb7c171a
kweonsunho <kweonsunho7@gmail.com>
2017-08-09 12:46:01 +0900

    by me

--- Hello world/src/pushprint.java
+++ Hello world/src/pushprint.java
@@ -1,13 +1,9 @@
 
 public class pushprint{
 	public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("add java class file push");
 	System.out.println("world with me");
-=======
-	System.out.println("world's end dancehall");
 		System.out.println("world with you");
->>>>>>> refs/remotes/origin/master
 	}
 }
 

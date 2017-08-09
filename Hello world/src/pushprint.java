73f7fd927fb0cbf41eda283746be59f0d19c7c2b
kweonsunho <kweonsunho7@gmail.com>
2017-08-09 12:41:34 +0900

    pushprint.java modified on local reposirory

--- Hello world/src/pushprint.java
+++ Hello world/src/pushprint.java
@@ -1,7 +1,8 @@
 
 public class pushprint{
 	public static void main(String[] args) {
-	System.out.println("world's end dancehall");
+	System.out.println("add java class file push");
+	System.out.println("world with me");
 	}
 }
 
